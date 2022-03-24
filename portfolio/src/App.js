962bc8c8eee4452937f9f223e354b247ba94af02
Mark Alfano <mpalfano4@gmail.com>
2022-03-24 16:39:39 -0400

    updated header nav

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Portfolio from "./components/pages/Portfolio";
 import Contact from "./components/pages/Contact";
 import Home from "./components/pages/Home";
+import Resume from "./components/pages/Resume";
 
 const App = () => (
   <BrowserRouter>
@@ -12,6 +13,7 @@ const App = () => (
       <Route path="/" element={<Home />} exact />
       <Route path="/Contact" element={<Contact />} exact />
       <Route path="/Portfolio" element={<Portfolio />} exact />
+      <Route path="/Resume" element={<Resume />} exact />
     </Routes>
   </BrowserRouter>
 );
--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -32,7 +32,9 @@ const Header = () => {
         <Link className="p-4" to="Portfolio">
           Portfolio
         </Link>
-        <Link to="Contact">Contact</Link>
+        <Link className="p-4" to="Contact">
+          Contact
+        </Link>
         <Link className="p-4" to="Resume">
           Resume
         </Link>
@@ -42,43 +44,3 @@ const Header = () => {
 };
 
 export default Header;
-
-// import React from "react";
-
-// const NavBar = ({ currentPage, handlePageChange }) => {
-//   return (
-//     <ul className="nav nav-Bar">
-//       <li className="nav-item">
-//         <a
-//           href="#home"
-//           onClick={() => handlePageChange("Home")}
-//           className={currentPage === "Home" ? "nav-link active" : "nav-link"}
-//         >
-//           Home
-//         </a>
-//       </li>
-
-//       <li className="nav-item">
-//         <a
-//           href="#projects"
-//           onClick={() => handlePageChange("Project")}
-//           className={currentPage === "Project" ? "nav-link active" : "nav-link"}
-//         >
-//           Project
-//         </a>
-//       </li>
-
-//       <li className="nav-item">
-//         <a
-//           href="#contact"
-//           onClick={() => handlePageChange("Contact")}
-//           className={currentPage === "Contact" ? "nav-link active" : "nav-link"}
-//         >
-//           Contact
-//         </a>
-//       </li>
-//     </ul>
-//   );
-// };
-
-// export default NavBar;
