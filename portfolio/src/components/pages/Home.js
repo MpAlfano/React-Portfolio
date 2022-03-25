83b9f21695dfb0934219497fbca985dbc64b3aaf
Mark Alfano <mpalfano4@gmail.com>
2022-03-25 00:36:03 -0400

    small changes to flexbox

--- portfolio/src/components/pages/Home.js
+++ portfolio/src/components/pages/Home.js
@@ -3,24 +3,26 @@ import profPicture from "..//images/bio.jpg";
 
 export default function Home() {
   return (
-    <div className="md:container md:mx-auto">
-      <img
-        className="w-32 md:w-64 lg:w-96"
-        src={profPicture}
-        alt="biography"
-      ></img>
-      <h1>About Me:</h1>
-      <p>
-        Hello my name is Mark, I previously ran my own landscaping business for
-        over seven years before deciding I wanted to pursue coding. I am very
-        goal orientated and enjoy the planning process almost as much as doing
-        or completing a project. I have always been intrigued by programming and
-        the works going on behind the scene on webpages and mobile apps. Now
-        that I have begun my journey on this path I am creating many works of my
-        own. I really enjoy creating new things and thinking of new ways to
-        complete tasks more efficiently. I look forward to my future and maybe
-        one day I will be working with you!
-      </p>
+    <div className="container mt-5 md:mx-auto bg-blue-400 rounded-log">
+      <div className="md:flex">
+        <img
+          className="float-left w-32 md:w-64 lg:w-96"
+          src={profPicture}
+          alt="biography"
+        ></img>
+        <h1>About Me:</h1>
+        <p>
+          Hello my name is Mark, I previously ran my own landscaping business
+          for over seven years before deciding I wanted to pursue coding. I am
+          very goal orientated and enjoy the planning process almost as much as
+          doing or completing a project. I have always been intrigued by
+          programming and the works going on behind the scene on webpages and
+          mobile apps. Now that I have begun my journey on this path I am
+          creating many works of my own. I really enjoy creating new things and
+          thinking of new ways to complete tasks more efficiently. I look
+          forward to my future and maybe one day I will be working with you!
+        </p>
+      </div>
     </div>
   );
 }
