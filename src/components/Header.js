b3361435dbf28800635f6760a304a27b32cfb6aa
Mark Alfano <mpalfano4@gmail.com>
2022-03-27 23:55:23 -0400

    updating nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,17 +28,17 @@ const Header = ({ toggle }) => {
       <div className="pr-8 md:block hidden">
         <Link className="inline-flex items-center pr-10" to="/React-Portfolio">
           <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
+            className="w-6 h-6"
             fill="none"
-            viewBox="0 0 24 24"
             stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
           >
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth={2}
-              d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
+              d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
             />
           </svg>
           About Me
