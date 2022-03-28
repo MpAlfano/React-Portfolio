08d0aa025f9f05bfef8bb8267daa66856d29d608
Mark Alfano <mpalfano4@gmail.com>
2022-03-27 21:09:36 -0400

    added emailjs

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,42 +1,119 @@
-import React from "react";
+// import React from "react";
+
+// export default function Contact() {
+//   return (
+//     <div className="container px-5 my-5">
+//       <form id="contactForm">
+//         <div className="mb-3">
+//           <label className="form-label" htmlFor="name">
+//             Name
+//           </label>
+//           <input
+//             className="form-control"
+//             id="name"
+//             type="text"
+//             placeholder="Name"
+//             data-sb-validations=""
+//           />
+//         </div>
+//         <div className="mb-3">
+//           <label className="form-label" htmlFor="emailAddress">
+//             Email Address
+//           </label>
+//           <input
+//             className="form-control"
+//             id="emailAddress"
+//             type="email"
+//             placeholder="Email Address"
+//             data-sb-validations="required,email"
+//           />
+//           <div
+//             className="invalid-feedback"
+//             data-sb-feedback="emailAddress:required"
+//           >
+//             Email Address is required.
+//           </div>
+//           <div
+//             className="invalid-feedback"
+//             data-sb-feedback="emailAddress:email"
+//           >
+//             Email Address Email is not valid.
+//           </div>
+//         </div>
+//         <div className="mb-3">
+//           <label className="form-label" htmlFor="message">
+//             Message
+//           </label>
+//           <input
+//             className="form-control"
+//             id="message"
+//             type="text"
+//             placeholder="Message"
+//             data-sb-validations="required"
+//           />
+//           <div className="invalid-feedback" data-sb-feedback="message:required">
+//             Message is required.
+//           </div>
+//         </div>
+//         <div className="d-none" id="submitSuccessMessage">
+//           <div className="text-center mb-3">
+//             <div className="fw-bolder">Form submission successful!</div>
+//           </div>
+//         </div>
+//         <div className="d-none" id="submitErrorMessage">
+//           <div className="text-center text-danger mb-3">
+//             Error sending message!
+//           </div>
+//         </div>
+//         <div className="d-grid">
+//           <button
+//             className="btn btn-primary btn-lg"
+//             id="submitButton"
+//             type="submit"
+//           >
+//             Submit
+//           </button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// }
+
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 export default function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_px2fv3w",
+        "template_13eizjr",
+        form.current,
+        "2PI30PYVsL2Y8fZ6W"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
-    <div className="container mt-5 mx-auto px-2">
-      <div className="md:flex">
-        <div className="flex-1 text-black text-center px-5 py-5">
-          <div>
-            <h1>Fill out this form to send me an email.</h1>
-            <form className="">
-              <div>
-                <label>Name:</label>
-                <br></br>
-                <input className="w-1/2" type="text" id="name" />
-              </div>
-              <div>
-                <label>Email:</label>
-                <br></br>
-                <input
-                  className="w-1/2 required:border-red-500 "
-                  type="text"
-                  id="email"
-                />
-              </div>
-              <div>
-                <label>Message</label>
-                <br></br>
-                <input className="w-full h-36" type="text" id="message" />
-              </div>
-              <div className="mb-2">
-                <br></br>
-                <button className="btn color-green" type="submit">
-                  Signup
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
   );
 }
