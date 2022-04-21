d152566b157f4b37d1fc0e118cb00bfdd07c63ac
Mark Alfano <mpalfano4@gmail.com>
2022-04-21 17:02:06 -0400

    picture change

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -17,7 +17,11 @@ export default function Portfolio() {
           <div className="flex-1 text-black text-center bg-teal-400 hover:border-dashed border-2 border-indigo-600 px-2 lg:px-5 py-2 lg:py-5 m-2 rounded">
             <div className="lg:flex lg:items-center">
               <div className="lg:flex-shrink-0">
-                <img className="rounded-lg lg:w-64" src={gitfit} alt="GitFit" />
+                <img
+                  className="rounded-lg lg:w-64 lg:h-48"
+                  src={gitfit}
+                  alt="GitFit"
+                />
                 <h1 className="font-bold">Technologies Used:</h1>
                 <ul className="grid grid-cols-2">
                   <li>MERN Stack</li>
@@ -63,61 +67,6 @@ export default function Portfolio() {
               </div>
             </div>
           </div>
-          <div className="flex-1 text-black text-center bg-teal-400 hover:border-dashed border-2 border-indigo-600 px-2 lg:px-5 py-2 lg:py-5 m-2 rounded">
-            <div className="lg:flex lg:items-center">
-              <div className="lg:flex-shrink-0">
-                <img
-                  className="rounded-lg lg:w-64"
-                  src={proj1}
-                  alt="Pick & Mix"
-                />
-                <h1 className="font-bold">Technologies Used:</h1>
-                <ul className="grid grid-cols-2">
-                  <li>MySql</li>
-                  <li>Node</li>
-                  <li>Express</li>
-                  <li>REST</li>
-                  <li>Sequelize</li>
-                  <li>Handlebars</li>
-                  <li>JavaScript</li>
-                  <li>Bootstrap</li>
-                </ul>
-              </div>
-              <div className="mt-4 lg:mt-0 lg:ml-6">
-                <div className="uppercase tracking-wide text-md font-bold">
-                  Pick & Mix
-                </div>
-                <p>
-                  Full-stack project that uses a database to populate grocery
-                  items for a user to browse through. A user can sign up and
-                  begin adding items to their list. The user can also view that
-                  list to update item quantity, remove items and also remove all
-                  the items. Other features include the current date being
-                  displayed and a search bar to filter items.
-                </p>
-                <div className="grid grid-cols-2">
-                  <a
-                    href="https://pick-n-mix.herokuapp.com/"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="block mt-1 text-lg leading-tight font-semibold hover:text-sky-500"
-                  >
-                    Project
-                  </a>
-                  <a
-                    href="https://github.com/MpAlfano/pick-n-mix"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="block mt-1 text-lg leading-tight font-semibold hover:text-sky-500"
-                  >
-                    Github Repo
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="md:flex">
           <div className="flex-1 text-black text-center bg-teal-400 hover:border-dashed border-2 border-indigo-600 px-2 lg:px-5 py-2 lg:py-5 m-2 rounded">
             <div className="lg:flex lg:items-center">
               <div className="lg:flex-shrink-0">
@@ -169,11 +118,66 @@ export default function Portfolio() {
               </div>
             </div>
           </div>
+        </div>
+        <div className="md:flex">
           <div className="flex-1 text-black text-center bg-teal-400 hover:border-dashed border-2 border-indigo-600 px-2 lg:px-5 py-2 lg:py-5 m-2 rounded">
             <div className="lg:flex lg:items-center">
               <div className="lg:flex-shrink-0">
                 <img
-                  className="rounded-lg lg:w-64"
+                  className="rounded-lg lg:w-64 lg:h-48"
+                  src={proj1}
+                  alt="Pick & Mix"
+                />
+                <h1 className="font-bold">Technologies Used:</h1>
+                <ul className="grid grid-cols-2">
+                  <li>MySql</li>
+                  <li>Node</li>
+                  <li>Express</li>
+                  <li>REST</li>
+                  <li>Sequelize</li>
+                  <li>Handlebars</li>
+                  <li>JavaScript</li>
+                  <li>Bootstrap</li>
+                </ul>
+              </div>
+              <div className="mt-4 lg:mt-0 lg:ml-6">
+                <div className="uppercase tracking-wide text-md font-bold">
+                  Pick & Mix
+                </div>
+                <p>
+                  Full-stack project that uses a database to populate grocery
+                  items for a user to browse through. A user can sign up and
+                  begin adding items to their list. The user can also view that
+                  list to update item quantity, remove items and also remove all
+                  the items. Other features include the current date being
+                  displayed and a search bar to filter items.
+                </p>
+                <div className="grid grid-cols-2">
+                  <a
+                    href="https://pick-n-mix.herokuapp.com/"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="block mt-1 text-lg leading-tight font-semibold hover:text-sky-500"
+                  >
+                    Project
+                  </a>
+                  <a
+                    href="https://github.com/MpAlfano/pick-n-mix"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="block mt-1 text-lg leading-tight font-semibold hover:text-sky-500"
+                  >
+                    Github Repo
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex-1 text-black text-center bg-teal-400 hover:border-dashed border-2 border-indigo-600 px-2 lg:px-5 py-2 lg:py-5 m-2 rounded">
+            <div className="lg:flex lg:items-center">
+              <div className="lg:flex-shrink-0">
+                <img
+                  className="rounded-lg lg:w-64 "
                   src={proj3}
                   alt="Team Profile Generator"
                 />
