a1de27efe3d3de3492b5b6619ed600c11717940e
Mark Alfano <mpalfano4@gmail.com>
2022-04-20 22:32:16 -0400

    cleaning up code, adding skills

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,35 +3,6 @@ import profPicture from "..//images/bio.jpg";
 
 export default function Home() {
   return (
-    // <div>
-    //   <div className="md:flex justify-center">
-    //     <div className="profDiv md:flex text-center margin: 0 auto px-2 md:px-10 lg:px-40">
-    //       <h1 className="text-center font-bold text-4xl text-black">
-    //         About Me
-    //       </h1>
-    //       <br></br>
-    //       <img className="profPic" src={profPicture} alt="biography"></img>
-    //       <section>
-    //         <div className="bg-sky-200">
-    //           <p>
-    //             Hello my name is Mark, I'm an ambitious Full-Stack Developer
-    //             looking for new opportunities to grow and enjoy tackling new
-    //             challenges. Previously I operated my owned landscaping business
-    //             for over seven years before deciding I wanted to pursue coding.
-    //             I am very goal orientated and enjoy the planning process almost
-    //             as much as working on or completing a project. I have always
-    //             been intrigued by programming. With interests in how everything
-    //             is bulit and what other new apps could be made to improve our
-    //             way of life. I really enjoy creating new things and thinking of
-    //             new ways to complete tasks more efficiently. I look forward to
-    //             my future and maybe one day I will be working with you.
-    //           </p>
-    //         </div>
-    //       </section>
-    //     </div>
-    //   </div>
-    // </div>
-
     <div>
       <h1 className="text-center font-bold text-xl lg:text-4xl text-black">
         About Me
@@ -47,35 +18,21 @@ export default function Home() {
                   alt="GitFit"
                 />
               </div>
-
-              <div className="mt-4 lg:mt-0 lg:ml-6">
-                <div className="uppercase tracking-wide text-md font-bold">
-                  GitFit
-                </div>
+              <div className="mt-4 px-2">
                 <p>
-                  MERN stack application utliizing GraphQL and apollo-client to
-                  access a complex database. Database structure includes users,
-                  exercises, workout plans with weekly and daliy exercises.
-                  Users can sign up and check out workout plans, add them to
-                  their own proflie. Users can also view exercises with
-                  descriptions and an embedded how to video.
+                  Hello my name is Mark, I'm an ambitious Full-Stack Developer
+                  looking for new opportunities to grow and enjoy tackling new
+                  challenges. Previously I operated my owned landscaping
+                  business for over seven years before deciding I wanted to
+                  pursue coding. I am very goal orientated and enjoy the
+                  planning process almost as much as working on or completing a
+                  project. I have always been intrigued by programming. With
+                  interests in how everything is bulit and what other new apps
+                  could be made to improve our way of life. I really enjoy
+                  creating new things and thinking of new ways to complete tasks
+                  more efficiently. I look forward to my future and maybe one
+                  day I will be working with you.
                 </p>
-                <a
-                  href="https://gitfit-2.herokuapp.com/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="block mt-1 text-lg leading-tight font-semibold hover:text-sky-300"
-                >
-                  Project
-                </a>
-                <a
-                  href="https://github.com/nkrliis/GitFit-2.0"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="block mt-1 text-lg leading-tight font-semibold hover:text-sky-300"
-                >
-                  Github Repo
-                </a>
               </div>
             </div>
             <div className="">
@@ -85,8 +42,9 @@ export default function Home() {
                   <h1 className="text-lg font-bold">Languages & Tools</h1>
                   <ul>
                     <li>JavaScript</li>
-                    <li>SQL& NoSQL</li>
+                    <li>MySQL & NoSQL</li>
                     <li>Git</li>
+                    <li>Test Driven Development</li>
                   </ul>
                 </div>
 
