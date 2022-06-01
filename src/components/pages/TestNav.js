497bd317cc26717eb156df5d6761a68bf2011d3b
Mark Alfano <mpalfano4@gmail.com>
2022-06-01 14:50:41 -0400

    navbar links for each page added

--- src/components/pages/TestNav.js
+++ src/components/pages/TestNav.js
@@ -1,13 +1,27 @@
 import React from "react";
 import { Link, animateScroll } from "react-scroll";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import Bio from "../images/favicon.png";
 
 export default function TestNav() {
   return (
-    <div className="text-blue-500 z-50 navbar">
+    <nav className="w-24 h-screen fixed top-0 text-blue-500 z-50">
       <div className="navItem">
         <Link
-          classname=""
+          activeClass="active"
+          to="home"
+          spy={true}
+          smooth={true}
+          offset={0}
+          duration={500}
+        >
+          <img src={Bio} alt="name" />
+          {/* About */}
+        </Link>
+        <Link
           activeClass="active"
           to="about"
           spy={true}
@@ -15,11 +29,10 @@ export default function TestNav() {
           offset={0}
           duration={500}
         >
-          <img src={Bio} alt="name" />
-          About
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+          {/* Portfolio */}
         </Link>
         <Link
-          classname=""
           activeClass="active"
           to="portfolio"
           spy={true}
@@ -27,11 +40,10 @@ export default function TestNav() {
           offset={0}
           duration={500}
         >
-          <img src={Bio} alt="name" />
-          Portfolio
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+          {/* Portfolio */}
         </Link>
         <Link
-          classname=""
           activeClass="active"
           to="portfolio2"
           spy={true}
@@ -39,11 +51,21 @@ export default function TestNav() {
           offset={0}
           duration={500}
         >
-          <img src={Bio} alt="name" />
-          Portfolio
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+          {/* Portfolio */}
         </Link>
         <Link
-          classname=""
+          activeClass="active"
+          to="resume"
+          spy={true}
+          smooth={true}
+          offset={0}
+          duration={500}
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+          {/* Contact */}
+        </Link>
+        <Link
           activeClass="active"
           to="contact"
           spy={true}
@@ -51,10 +73,38 @@ export default function TestNav() {
           offset={0}
           duration={500}
         >
-          <img src={Bio} alt="name" />
-          Contact
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+          {/* Contact */}
         </Link>
       </div>
-    </div>
+
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/markalfano"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/MpAlfano"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} color="4d4d4e" />
+          </a>
+        </li>
+        <a href="mailto: mpalfano4@gmail.com" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faEnvelope} color="4d4d4e" />
+        </a>
+
+        <li>
+          <FontAwesomeIcon icon={faPhone} color="4d4d4e" />
+        </li>
+      </ul>
+    </nav>
   );
 }
