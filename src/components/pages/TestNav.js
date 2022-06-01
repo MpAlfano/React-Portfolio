0fa9b8054a04566ed3a06cca51313abd742e72d7
Mark Alfano <mpalfano4@gmail.com>
2022-06-01 12:37:02 -0400

    switching to sass for styling and adding animate.css for animations

--- src/components/pages/TestNav.js
+++ src/components/pages/TestNav.js
@@ -1,59 +1,60 @@
 import React from "react";
 import { Link, animateScroll } from "react-scroll";
+import Bio from "../images/favicon.png";
 
 export default function TestNav() {
   return (
-    <div className="fixed top-0 w-full inline z-50">
-      <ul className="flex justify-end p-2 pr-10 font-bold text-blue-500">
-        <li className="px-2 navbar">
-          <Link
-            activeClass="active"
-            to="about"
-            spy={true}
-            smooth={true}
-            offset={0}
-            duration={500}
-          >
-            About
-          </Link>
-        </li>
-        <li className="px-2 navbar">
-          <Link
-            activeClass="active"
-            to="portfolio"
-            spy={true}
-            smooth={true}
-            offset={0}
-            duration={500}
-          >
-            Portfolio
-          </Link>
-        </li>
-        <li className="px-2 navbar">
-          <Link
-            activeClass="active"
-            to="portfolio2"
-            spy={true}
-            smooth={true}
-            offset={0}
-            duration={500}
-          >
-            Portfolio
-          </Link>
-        </li>
-        <li className="px-2 navbar">
-          <Link
-            activeClass="active"
-            to="contact"
-            spy={true}
-            smooth={true}
-            offset={0}
-            duration={500}
-          >
-            Contact
-          </Link>
-        </li>
-      </ul>
+    <div className="text-blue-500 z-50 navbar">
+      <div className="navItem">
+        <Link
+          classname=""
+          activeClass="active"
+          to="about"
+          spy={true}
+          smooth={true}
+          offset={0}
+          duration={500}
+        >
+          <img src={Bio} alt="name" />
+          About
+        </Link>
+        <Link
+          classname=""
+          activeClass="active"
+          to="portfolio"
+          spy={true}
+          smooth={true}
+          offset={0}
+          duration={500}
+        >
+          <img src={Bio} alt="name" />
+          Portfolio
+        </Link>
+        <Link
+          classname=""
+          activeClass="active"
+          to="portfolio2"
+          spy={true}
+          smooth={true}
+          offset={0}
+          duration={500}
+        >
+          <img src={Bio} alt="name" />
+          Portfolio
+        </Link>
+        <Link
+          classname=""
+          activeClass="active"
+          to="contact"
+          spy={true}
+          smooth={true}
+          offset={0}
+          duration={500}
+        >
+          <img src={Bio} alt="name" />
+          Contact
+        </Link>
+      </div>
     </div>
   );
 }
