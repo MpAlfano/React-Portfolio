b88de490a9a4e4048a68a1f67e9e70b6e05a82b6
Mark Alfano <mpalfano4@gmail.com>
2022-06-02 22:22:19 -0400

    added in useEffect and scrolling to change color of navbar

--- src/components/pages/TestNav.js
+++ src/components/pages/TestNav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link, animateScroll } from "react-scroll";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome, faEnvelope } from "@fortawesome/free-solid-svg-icons";
@@ -7,9 +7,66 @@ import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import Bio from "../images/favicon.png";
 
 export default function TestNav() {
+  const prevScrollY = useRef(0);
+  const { innerWidth: width, innerHeight: height } = window;
+
+  const [goingUp, setGoingUp] = useState(false);
+  const [background, setBackground] = useState(
+    "w-20 h-screen fixed bg-gradient-to-b from-neutral-500 via-green-400 to-neutral-500 top-0 z-50"
+  );
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      if (prevScrollY.current < currentScrollY && goingUp) {
+        setGoingUp(false);
+      }
+      if (prevScrollY.current > currentScrollY && !goingUp) {
+        setGoingUp(true);
+      }
+
+      if (currentScrollY < height) {
+        setBackground(
+          "w-20 h-screen fixed bg-gradient-to-b from-neutral-500 via-green-400 to-neutral-500 top-0 z-50"
+        );
+      }
+      if (currentScrollY >= height && currentScrollY < height * 2) {
+        console.log(true);
+        setBackground(
+          "w-20 h-screen fixed bg-gradient-to-b from-neutral-500 via-blue-400 to-neutral-500 top-0 z-50"
+        );
+      }
+      if (currentScrollY >= height * 2 && currentScrollY < height * 3) {
+        setBackground(
+          "w-20 h-screen fixed bg-gradient-to-b from-neutral-500 via-purple-400 to-neutral-500 top-0 z-50"
+        );
+      }
+      if (currentScrollY >= height * 3 && currentScrollY < height * 4) {
+        setBackground(
+          "w-20 h-screen fixed bg-gradient-to-b from-neutral-500 via-red-400 to-neutral-500 top-0 z-50"
+        );
+      }
+      if (currentScrollY >= height * 4 && currentScrollY < height * 5) {
+        setBackground(
+          "w-20 h-screen fixed bg-gradient-to-b from-neutral-500 via-orange-400 to-neutral-500 top-0 z-50"
+        );
+      }
+      if (currentScrollY >= height * 5 && currentScrollY < height * 6) {
+        setBackground(
+          "w-20 h-screen fixed bg-gradient-to-b from-neutral-500 via-yellow-400 to-neutral-500 top-0 z-50"
+        );
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [goingUp]);
+
   return (
-    <nav className="w-20 h-screen fixed top-0 bg-gradient-to-b from-neutral-500 via-cyan-400 to-neutral-500 z-50">
+    <nav className={background} id="nav">
       <Link
+        id="bg-home"
         activeClass="active"
         to="home"
         spy={true}
@@ -18,10 +75,10 @@ export default function TestNav() {
         duration={500}
       >
         <img src={Bio} alt="name" />
-        {/* About */}
       </Link>
       <div className="navItem">
         <Link
+          id="bg-about"
           activeClass="active"
           to="about"
           spy={true}
@@ -30,9 +87,9 @@ export default function TestNav() {
           duration={500}
         >
           <FontAwesomeIcon icon={faHome} color="#afc0c4" />
-          {/* Portfolio */}
         </Link>
         <Link
+          id="bg-proj1"
           activeClass="active"
           to="portfolio"
           spy={true}
@@ -41,9 +98,9 @@ export default function TestNav() {
           duration={500}
         >
           <FontAwesomeIcon icon={faHome} color="#afc0c4" />
-          {/* Portfolio */}
         </Link>
         <Link
+          id="bg-proj2"
           activeClass="active"
           to="portfolio2"
           spy={true}
@@ -52,9 +109,9 @@ export default function TestNav() {
           duration={500}
         >
           <FontAwesomeIcon icon={faHome} color="#afc0c4" />
-          {/* Portfolio */}
         </Link>
         <Link
+          id="bg-resume"
           activeClass="active"
           to="resume"
           spy={true}
@@ -63,9 +120,9 @@ export default function TestNav() {
           duration={500}
         >
           <FontAwesomeIcon icon={faEnvelope} color="#afc0c4" />
-          {/* Contact */}
         </Link>
         <Link
+          id="bg-contact"
           activeClass="active"
           to="contact"
           spy={true}
@@ -74,7 +131,6 @@ export default function TestNav() {
           duration={500}
         >
           <FontAwesomeIcon icon={faEnvelope} color="#afc0c4" />
-          {/* Contact */}
         </Link>
       </div>
       <ul className="contacts">
