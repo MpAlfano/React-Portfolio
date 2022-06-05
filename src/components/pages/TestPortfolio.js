33a6a50e65961599fac79237aa3c61fb428d225a
Mark Alfano <mpalfano4@gmail.com>
2022-06-05 19:07:04 -0400

    redoing some gradient colour schemes and changed project layouts

--- src/components/pages/TestPortfolio.js
+++ src/components/pages/TestPortfolio.js
@@ -1,50 +1,25 @@
 import React from "react";
-import proj1 from "../images/proj1.png";
-import proj2 from "../images/proj2.png";
-import proj5 from "../images/proj5.png";
-import gitfit from "../images/gitfit.png";
 
 export default function TestPortfolio() {
   return (
-    <div className="sm:h-fit md:h-screen" id="portfolio">
+    <div
+      className="grid content-center sm:h-fit md:h-screen bg-gradient-to-b from-green-200 to-purple-100"
+      id="portfolio"
+    >
       <div className="container md:pl-20 mx-auto">
-        <div className="grid pt-2 md:pt-10 lg:pt-20 m-auto">
-          <div className="flex-1 text-black text-center bg-teal-400 hover:border-dashed border-2 border-indigo-600 px-2 lg:px-5 py-2 lg:py-5 m-2 rounded-lg">
+        <div className="grid md:gap-y-4 lg:gap-y-10 m-auto">
+          <div className="flex-1 text-black text-center border border-indigo-600 px-2 lg:px-5 py-2 lg:py-5 m-2 rounded-lg">
+            <div className="uppercase tracking-wide text-md text-2xl font-bold lg:pb-2">
+              GitFit
+            </div>
             <div className="lg:flex lg:items-center">
-              <div className="lg:flex-shrink-0">
-                <img
-                  className="rounded-lg w-32 lg:w-64"
-                  src={gitfit}
-                  alt="GitFit"
-                />
-                <h1 className="font-bold">Technologies Used:</h1>
-                <ul className="grid grid-cols-3 md:grid-cols-2">
-                  <li>MERN Stack</li>
-                  <li>GraphQL</li>
-                  <li>Apollo</li>
-                  <li>React</li>
-                  <li>JavaScript</li>
-                  <li>TailwindCSS</li>
-                </ul>
-              </div>
               <div className="mt-4 lg:mt-0 lg:ml-6">
-                <div className="uppercase tracking-wide text-md font-bold">
-                  GitFit
-                </div>
-                <p>
-                  MERN stack application utilizing GraphQL and apollo-client to
-                  access a complex database. Database structure includes users,
-                  exercises, workout plans with weekly and daily exercises.
-                  Users can sign up and check out workout plans, add them to
-                  their own profile. Users can also view exercises with
-                  descriptions and an embedded how to video.
-                </p>
-                <div className="grid grid-cols-2">
+                <div className="flex flex-1 h-32 lg:h-52 w-full pb-2 px-12">
                   <a
                     href="https://gitfit-2.herokuapp.com/"
                     target="_blank"
                     rel="noreferrer"
-                    className="block mt-1 text-lg leading-tight font-semibold hover:text-sky-500"
+                    className="bg-project1 w-full block mt-1 text-lg leading-tight font-semibold"
                   >
                     Project
                   </a>
@@ -52,37 +27,60 @@ export default function TestPortfolio() {
                     href="https://github.com/nkrilis/GitFit-2.0"
                     target="_blank"
                     rel="noreferrer"
-                    className="block mt-1 text-lg leading-tight font-semibold hover:text-sky-500"
+                    className="bg-repo w-full block mt-1 text-lg leading-tight font-semibold"
                   >
                     Github Repo
                   </a>
                 </div>
+                <p className="text-lg lg:text-2xl">
+                  MERN stack application utilizing GraphQL and apollo-client to
+                  access a complex database. Database structure includes users,
+                  exercises, workout plans with weekly and daily exercises.
+                  Users can sign up and check out workout plans, add them to
+                  their own profile. Users can also view exercises with
+                  descriptions and an embedded how to video.
+                </p>
+              </div>
+              <div className="lg:flex-shrink-0 px-8 lg:w-80">
+                <h1 className="font-bold text-2xl pb-5">Technologies Used:</h1>
+                <ul className="grid grid-cols-4 lg:grid-cols-2 lg:text-xl font-semibold text-center">
+                  <li>MERN Stack</li>
+                  <li>GraphQL</li>
+                  <li>Apollo</li>
+                  <li>React</li>
+                  <li>JavaScript</li>
+                  <li>TailwindCSS</li>
+                  <li>Express</li>
+                  <li>Node</li>
+                </ul>
               </div>
             </div>
           </div>
-          <div className="flex-1 text-black text-center bg-teal-400 hover:border-dashed border-2 border-indigo-600 px-2 lg:px-5 py-2 lg:py-5 m-2 rounded-lg">
+          <div className="flex-1 text-black text-center border border-indigo-600 px-2 lg:px-5 py-2 lg:py-5 m-2 rounded-lg">
+            <div className="uppercase tracking-wide text-md text-2xl font-bold pb-2">
+              Off The Couch
+            </div>
             <div className="lg:flex lg:items-center">
-              <div className="lg:flex-shrink-0">
-                <img
-                  className="rounded-lg w-32 lg:w-64"
-                  src={proj2}
-                  alt="Off The Couch"
-                />
-                <h1 className="font-bold">Technologies Used:</h1>
-                <ul className="grid grid-cols-3 md:grid-cols-2">
-                  <li>MapQuest API</li>
-                  <li>Bored API</li>
-                  <li>SerpStack API</li>
-                  <li>Bootstrap</li>
-                  <li>JavaScript</li>
-                  <li>JQuery</li>
-                </ul>
-              </div>
               <div className="mt-4 lg:mt-0 lg:ml-6">
-                <div className="uppercase tracking-wide text-md font-bold">
-                  Off The Couch
+                <div className="flex flex-1 h-32 lg:h-52 w-full pb-2 px-12">
+                  <a
+                    href="https://mpalfano.github.io/Off-the-couch-personal/"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="bg-project2 w-full block mt-1 text-lg leading-tight font-semibold"
+                  >
+                    Project
+                  </a>
+                  <a
+                    href="https://github.com/MpAlfano/Off-The-Couch"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="bg-repo w-full block mt-1 text-lg leading-tight font-semibold"
+                  >
+                    Github Repo
+                  </a>
                 </div>
-                <p>
+                <p className="text-lg lg:text-2xl">
                   Project that helps a user with limited free time find
                   something to do nearby. They can choose an activity from a
                   list or have one randomly generated. A hungry user can choose
@@ -90,24 +88,17 @@ export default function TestPortfolio() {
                   can't decide what to eat. Application uses API's to get
                   location, search google and generate a random actvity.
                 </p>
-                <div className="grid grid-cols-2">
-                  <a
-                    href="https://mpalfano.github.io/Off-the-couch-personal/"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="block mt-1 text-lg leading-tight font-semibold hover:text-sky-500"
-                  >
-                    Project
-                  </a>
-                  <a
-                    href="https://github.com/MpAlfano/Off-the-couch-personal"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="block mt-1 text-lg leading-tight font-semibold hover:text-sky-500"
-                  >
-                    Github Repo
-                  </a>
-                </div>
+              </div>
+              <div className="lg:flex-shrink-0 px-8 lg:w-80">
+                <h1 className="font-bold text-2xl pb-5">Technologies Used:</h1>
+                <ul className="grid grid-cols-3 lg:grid-cols-2 text-lg lg:text-xl font-semibold text-center">
+                  <li>MapQuest API</li>
+                  <li>Bored API</li>
+                  <li>SerpStack API</li>
+                  <li>Bootstrap</li>
+                  <li>JavaScript</li>
+                  <li>JQuery</li>
+                </ul>
               </div>
             </div>
           </div>
--- src/components/pages/TestPortfolio2.js
+++ src/components/pages/TestPortfolio2.js
@@ -1,22 +1,49 @@
 import React from "react";
-import proj1 from "../images/proj1.png";
-import proj5 from "../images/proj5.png";
 
 export default function TestPortfolio2() {
   return (
-    <div className="sm:h-fit md:h-screen bg-green-700" id="portfolio2">
+    <div
+      className="grid content-center sm:h-fit md:h-screen bg-gradient-to-b from-orange-200 to-green-100"
+      id="portfolio2"
+    >
       <div className="container md:pl-20 mx-auto">
-        <div className="grid pt-2 md:pt-10 lg:pt-20 m-auto">
-          <div className="flex-1 text-black text-center bg-teal-400 hover:border-dashed border-2 border-indigo-600 px-2 lg:px-5 py-2 lg:py-5 m-2 rounded-lg">
+        <div className="grid md:gap-y-4 lg:gap-y-10 m-auto">
+          <div className="flex-1 text-black text-center border border-indigo-600 px-2 lg:px-5 py-2 lg:py-5 m-2 rounded-lg">
+            <div className="uppercase tracking-wide text-md text-2xl font-bold pb-2">
+              Pick & Mix
+            </div>
             <div className="lg:flex lg:items-center">
-              <div className="lg:flex-shrink-0">
-                <img
-                  className="rounded-lg lg:w-64 lg:h-48"
-                  src={proj1}
-                  alt="Pick & Mix"
-                />
-                <h1 className="font-bold">Technologies Used:</h1>
-                <ul className="grid grid-cols-2">
+              <div className="mt-4 lg:mt-0 lg:ml-6">
+                <div className="flex flex-1 h-32 lg:h-52 w-full pb-2 px-12">
+                  <a
+                    href="https://pick-n-mix.herokuapp.com/"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="bg-project3 w-full block mt-1 text-lg leading-tight font-semibold"
+                  >
+                    Project
+                  </a>
+                  <a
+                    href="https://github.com/MpAlfano/pick-n-mix"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="bg-repo w-full block mt-1 text-lg leading-tight font-semibold"
+                  >
+                    Github Repo
+                  </a>
+                </div>
+                <p className="text-lg lg:text-2xl">
+                  Full-stack project that uses a database to populate grocery
+                  items for a user to browse through. A user can sign up and
+                  begin adding items to their list. The user can also view that
+                  list to update item quantity, remove an item or remove all the
+                  items. Other features include the current date being displayed
+                  and a search bar to filter items.
+                </p>
+              </div>
+              <div className="lg:flex-shrink-0 px-8 lg:w-80">
+                <h1 className="font-bold text-2xl pb-5">Technologies Used:</h1>
+                <ul className="grid grid-cols-4 lg:grid-cols-2 lg:text-xl font-semibold text-center">
                   <li>MySql</li>
                   <li>Node</li>
                   <li>Express</li>
@@ -27,75 +54,20 @@ export default function TestPortfolio2() {
                   <li>Bootstrap</li>
                 </ul>
               </div>
-              <div className="mt-4 lg:mt-0 lg:ml-6">
-                <div className="uppercase tracking-wide text-md font-bold">
-                  Pick & Mix
-                </div>
-                <p>
-                  Full-stack project that uses a database to populate grocery
-                  items for a user to browse through. A user can sign up and
-                  begin adding items to their list. The user can also view that
-                  list to update item quantity, remove an item or remove all the
-                  items. Other features include the current date being displayed
-                  and a search bar to filter items.
-                </p>
-                <div className="grid grid-cols-2">
-                  <a
-                    href="https://pick-n-mix.herokuapp.com/"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="block mt-1 text-lg leading-tight font-semibold hover:text-sky-500"
-                  >
-                    Project
-                  </a>
-                  <a
-                    href="https://github.com/MpAlfano/pick-n-mix"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="block mt-1 text-lg leading-tight font-semibold hover:text-sky-500"
-                  >
-                    Github Repo
-                  </a>
-                </div>
-              </div>
             </div>
           </div>
-          <div className="flex-1 text-black text-center bg-teal-400 hover:border-dashed border-2 border-indigo-600 px-2 lg:px-5 py-2 lg:py-5 m-2 rounded-lg">
+          <div className="flex-1 text-black text-center border border-indigo-600 px-2 lg:px-5 py-2 lg:py-5 m-2 rounded-lg">
+            <div className="uppercase tracking-wide text-md text-2xl font-bold pb-2">
+              Social Network API
+            </div>
             <div className="lg:flex lg:items-center">
-              <div className="lg:flex-shrink-0">
-                <img
-                  className="rounded-lg lg:w-64"
-                  src={proj5}
-                  alt="Social-Network-API"
-                />
-                <h1 className="font-bold">Technologies Used:</h1>
-                <ul className="grid grid-cols-2">
-                  <li>MongoDB</li>
-                  <li>Mongoose</li>
-                  <li>Node</li>
-                  <li>Express</li>
-                  <li>Javascript</li>
-                  <li>dayjs</li>
-                </ul>
-              </div>
               <div className="mt-4 lg:mt-0 lg:ml-6">
-                <div className="uppercase tracking-wide text-md font-bold">
-                  Social Network API
-                </div>
-                <p>
-                  Back-end application using mongoDB, mongoose and the express
-                  server. Application allows a user to store users, their
-                  friends, thoughts and reactions into a database. When a user
-                  or their thought or a reaction is updated or deleted it is
-                  updated in the database. Reactions can be added to thoughts
-                  and users can also add friends.
-                </p>
-                <div className="grid grid-cols-2">
+                <div className="flex flex-1 h-32 lg:h-52 w-full pb-2 px-12">
                   <a
                     href="https://drive.google.com/file/d/1DKWWVOzsYXXoT9VpF7elQkfQbeLTxfZC/view"
                     target="_blank"
                     rel="noreferrer"
-                    className="block mt-1 text-lg leading-tight font-semibold hover:text-sky-500"
+                    className="bg-project4 w-full block mt-1 text-lg leading-tight font-semibold"
                   >
                     Project
                   </a>
@@ -103,11 +75,30 @@ export default function TestPortfolio2() {
                     href="https://github.com/MpAlfano/Social-Network-API"
                     target="_blank"
                     rel="noreferrer"
-                    className="block mt-1 text-lg leading-tight font-semibold hover:text-sky-500"
+                    className="bg-repo w-full block mt-1 text-lg leading-tight font-semibold"
                   >
                     Github Repo
                   </a>
                 </div>
+                <p className="text-lg lg:text-2xl">
+                  Back-end application using mongoDB, mongoose and the express
+                  server. Application allows a user to store users, their
+                  friends, thoughts and reactions into a database. When a user
+                  or their thought or a reaction is updated or deleted it is
+                  updated in the database. Reactions can be added to thoughts
+                  and users can also add friends.
+                </p>
+              </div>
+              <div className="lg:flex-shrink-0 px-8 lg:w-80">
+                <h1 className="font-bold text-2xl pb-5">Technologies Used:</h1>
+                <ul className="grid grid-cols-3 lg:grid-cols-2 text-lg lg:text-xl font-semibold text-center">
+                  <li>MongoDB</li>
+                  <li>Mongoose</li>
+                  <li>Node</li>
+                  <li>Express</li>
+                  <li>Javascript</li>
+                  <li>dayjs</li>
+                </ul>
               </div>
             </div>
           </div>
