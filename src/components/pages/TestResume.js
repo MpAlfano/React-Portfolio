6a989c16de80e7b97caa6ce926442f775d048839
Mark Alfano <mpalfano4@gmail.com>
2022-08-02 12:40:04 -0400

    changing icons

--- src/components/pages/TestResume.js
+++ src/components/pages/TestResume.js
@@ -2,6 +2,9 @@ import React from "react";
 import resume from "../images/resume.png";
 // import resumeContd from "../images/resumeContd.png"; - Left in for resume when it exceeds one page add new image
 import resumeDL from "../documents/resume.pdf";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFileDownload } from "@fortawesome/free-solid-svg-icons";
+import { faArrowAltCircleRight } from "@fortawesome/free-regular-svg-icons";
 
 export default function TestResume() {
   return (
@@ -14,21 +17,29 @@ export default function TestResume() {
           <div className="flex-1 text-black text-center md:pl-20 pt-4">
             <div className="relative">
               <img
-                className="object-scale-down w-full"
+                className="object-fill w-full"
                 src={resume}
                 alt="biography"
               ></img>
               <a href={resumeDL} download>
-                <i className="absolute top-0 left-0 fa-solid fa-file-arrow-down fa-2x"></i>
+                {/* <i className="absolute top-0 left-0 fa-solid fa-file-arrow-down fa-2x z-2"></i> */}
+                <FontAwesomeIcon
+                  icon={faFileDownload}
+                  className="absolute top-0 left-0 fa-2x"
+                />
               </a>
               <a
                 href="https://docs.google.com/document/d/1OBvrRbjsU4XBIBU20Y7tHCKF7pcfPNET1sDf4flLhfE/edit?usp=sharing"
                 target="blank_"
               >
-                <i
-                  className="absolute top-0 right-0 fa fa-external-link fa-2x"
+                {/* <i
+                  className="absolute top-0 right-0 fa fa-external-link fa-2x z-2"
                   aria-hidden="true"
-                ></i>
+                ></i> */}
+                <FontAwesomeIcon
+                  icon={faArrowAltCircleRight}
+                  className="absolute top-0 right-0 fa-2x"
+                />
               </a>
               <br></br>
             </div>
